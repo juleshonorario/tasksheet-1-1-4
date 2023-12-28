ba8ce4d9d3783e192cd61812acedaa2d7ffa174c
juleshonorario <153901269+juleshonorario@users.noreply.github.com>
2023-12-28 01:15:46 +0000

    Tasksheet-1-1-4

---
+++ ClassGrade.java
@@ -0,0 +1,13 @@
+public class ClassGrade {
+    public static void main(String[] args) {
+        int Grade = 72;
+        if (Grade<= 100 && Grade >= 90){
+            System.out.println(" A Grade ");
+        }
+        else if (Grade<= 89 && Grade >= 80)
+            System.out.println(" B Grade ");
+        else if (Grade <= 79 && Grade >= 70)
+            System.out.println ( " C Grade ");
+    }
+}
+
